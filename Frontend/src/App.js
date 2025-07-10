5645310a303d70983cbef5e8eccbf8edb1fa7dba
Aneeshaik <aneesnafeesimmu@gmail.com>
2025-07-10 17:38:33 +0530

    changes

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -18,7 +18,7 @@ const App = () => {
         
         if (storedToken) {
             try{
-                const response = await fetch('https://quiz-app-bv7l.onrender.com/auth/check', {
+                const response = await fetch('https://quiz-app-api-umber.vercel.app/auth/check', {
                     headers:{
                         'Authorization': `Bearer ${storedToken}`
                     }
